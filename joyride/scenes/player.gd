2e29062f28021ddf4c206aabdd25e869c4666c66
Hei-Lima <heitor.lipeixoto@gmail.com>
2025-08-14 10:21:27 -0300

    Fix camera

--- joyride/scenes/player.gd
+++ joyride/scenes/player.gd
@@ -1,5 +1,15 @@
 extends CharacterBody2D
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+@export var initial_speed = 100
+@export var max_speed = 1000
+@export var increment = 0.1
+
+# Called when the node enters the scene tree for the first time.
 func _process(delta: float) -> void:
+	if Globals.current_speed + increment <= max_speed:
+		Globals.current_speed = snapped(Globals.current_speed + increment, 0.1)
+	else:
+		Globals.current_speed = max_speed
+		
 	position.x += Globals.current_speed * delta
+		
