03243a0a122206495972b0ee10e29e888eb9d840
Hei-Lima <heitor.lipeixoto@gmail.com>
2025-08-13 22:48:54 -0300

    adiciona mais coisas pro jetpack

---
+++ joyride/scenes/player.gd
@@ -0,0 +1,5 @@
+extends CharacterBody2D
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	position.x += Globals.current_speed * delta
--- joyride/scenes/scroll_controller.gd
+++ joyride/scenes/scroll_controller.gd
@@ -1,10 +1,12 @@
 extends Node2D
 
-const speed = 1000
+@export var initial_speed = 100
+@export var max_speed = 1000
+@export var increment = 0.1
 
 # Called when the node enters the scene tree for the first time.
 func _process(delta: float) -> void:
-	var axis = Input.get_axis("left", "right")
-	position.x += axis * speed * delta
-	print(axis)
-	print($Camera2D.global_position)
+	if Globals.current_speed < max_speed:
+		Globals.current_speed = snapped(Globals.current_speed + increment, 0.1)
+		position.x += Globals.current_speed * delta
+		print($Camera2D.global_position)
