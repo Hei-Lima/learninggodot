0c68710af9088adfc649cc38d123e0c29fd0d1eb
Hei-Lima <heitor.lipeixoto@gmail.com>
2025-08-14 23:35:48 -0300

    -m

--- joyride/scenes/player.gd
+++ joyride/scenes/player.gd
@@ -2,14 +2,25 @@ extends CharacterBody2D
 
 @export var initial_speed = 100
 @export var max_speed = 1000
-@export var increment = 0.1
+@export var increment = 0.9
+@export var jet_speed = 100
 
-# Called when the node enters the scene tree for the first time.
-func _process(delta: float) -> void:
+func _ready() -> void:
+	$AnimationPlayer.play("Running")
+
+func _physics_process(delta: float) -> void:
 	if Globals.current_speed + increment <= max_speed:
 		Globals.current_speed = snapped(Globals.current_speed + increment, 0.1)
 	else:
-		Globals.current_speed = max_speed
-		
+		Globals.current_speed = max_speed	
 	position.x += Globals.current_speed * delta
+	
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+	
+	if Input.is_action_pressed("Up"):
+		velocity.y -= jet_speed
+	else:
+		velocity.y = move_toward(velocity.y, 0, jet_speed)
 		
+	move_and_slide()
