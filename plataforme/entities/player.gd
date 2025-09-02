ec33c4311d9c9cbf74c30723d0ff307defba4c52
Hei-Lima <heitor.lipeixoto@gmail.com>
2025-09-01 22:47:03 -0300

    --wip-- [skip ci]

--- plataforme/entities/player.gd
+++ plataforme/entities/player.gd
@@ -10,15 +10,13 @@ var floor_position = Vector2.ZERO
 
 var is_jumping: bool = false
 var is_bouncing: bool = false
-var moving_plataform_increment: int = 0
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
 
 func _physics_process(delta: float) -> void:
 	# --- Moviment horizontal ---
 	var dir := int(Input.is_action_pressed("right")) - int(Input.is_action_pressed("left"))
-
+		
 	if Input.is_action_pressed("duck") and is_on_floor():
 		$PlayerSprite.play("duck")
 		$PlayerSprite.flip_h = (dir < 0) if dir != 0 else $PlayerSprite.flip_h
@@ -27,10 +25,10 @@ func _physics_process(delta: float) -> void:
 		$PlayerSprite.play("walking")
 		$PlayerSprite.speed_scale = abs(velocity.x) / speed
 		$PlayerSprite.flip_h = (dir < 0)
-		velocity.x = move_toward(velocity.x, (dir * speed) + moving_plataform_increment, slide_factor * delta)
+		velocity.x = move_toward(velocity.x, dir * speed, slide_factor * delta)
 	else:
 		$PlayerSprite.play("idle")
-		velocity.x = move_toward(velocity.x, moving_plataform_increment, slide_factor * delta)
+		velocity.x = move_toward(velocity.x, 0, slide_factor * delta)
 		print(get_floor_velocity())
 
 	if Input.is_action_pressed("jump") and is_on_floor() and not Input.is_action_pressed("duck"):
@@ -65,6 +63,7 @@ func _physics_process(delta: float) -> void:
 	if velocity.y < 0 or Input.is_action_pressed("duck"):
 		set_collision_mask_value(6, false)
 		
+		
 	move_and_slide()
 	Globals.player_pos = global_position
 
@@ -73,7 +72,7 @@ func bounce(strength: int = 400) -> void:
 	velocity.y = -strength
 	is_bouncing = true
 	$PlayerSprite.play("jump")
-
+	
 # --- Hit/invulnerable ---
 func hit():
 	if invulnerable:
@@ -102,3 +101,10 @@ func get_floor_velocity() -> float:
 		if col and col.get_normal().dot(Vector2.UP) > 0.9 and floor_velocity:
 			return floor_velocity
 	return 0.0
+
+func get_floor():
+	for i in get_slide_collision_count():
+		var col = get_slide_collision(i)
+		if col and col.get_normal().dot(Vector2.UP) > 0.9:
+			return col.get_collider()
+	return null
